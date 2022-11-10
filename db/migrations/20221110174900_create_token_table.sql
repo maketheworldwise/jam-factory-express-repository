95ebcce87e55123ac598f08a49b743165a712f89
Kevin Ahn <maketheworldwise@gmail.com>
2022-11-11 03:39:35 +0900

    Feature: DB 날짜 갱신 설정 및 Token 테이블 추가

---
+++ db/migrations/20221110174900_create_token_table.sql
@@ -0,0 +1,19 @@
+-- migrate:up
+CREATE TABLE `TOKEN` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `user_id` bigint,
+  `access_token` text,
+  `refresh_token` text,
+  `ip` text,
+  `device` varchar(100) DEFAULT "web",
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE `TOKEN` ADD UNIQUE (`user_id`);
+ALTER TABLE `TOKEN` ADD FOREIGN KEY (`user_id`) REFERENCES `USER` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+-- migrate:down
+DROP TABLE `TOKEN`;
+
