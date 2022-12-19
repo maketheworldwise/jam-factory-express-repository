19df693f8dbbf301d7bf68d91a0694e7271da08e
Kevin Ahn <maketheworldwise@gmail.com>
2022-12-19 21:51:13 +0900

    Feature: 제품 장바구니 목록 조회 API 구현
    - 제품 장바구니 등록 로직 수정
    - 제품 장바구니 목록 조회 로직 추가
    - 제품 장바구니 목록 조회 예외 클래스 추가
    - 제품 장바구니 목록 조회 메세지 추가
    - 제품 장바구니 목록 조회 테스트 코드 추가

---
+++ src/main/exceptions/product/ProductCartFetchFailedException.ts
@@ -0,0 +1,10 @@
+import RunTimeException from '../RunTimeException';
+import statusCode from '../../utils/resStatusCode';
+
+class ProductCartFetchFailedException extends RunTimeException {
+  constructor(message: string) {
+    super(statusCode.BAD_REQUEST, message);
+  }
+}
+
+export default ProductCartFetchFailedException;
